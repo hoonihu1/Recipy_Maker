112e7831fee5403bd681a418e2d423a923131d34
donghass <ehdgk1221@gmail.com>
2024-05-26 22:05:16 +0900

    24-05-26

---
+++ recipy_maker/src/main/java/com/recipy/recipy_maker/RecipyMakerApplication.java
@@ -0,0 +1,13 @@
+package com.recipy.recipy_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipyMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipyMakerApplication.class, args);
+	}
+
+}
---
+++ recipy_maker/src/test/java/com/recipy/recipy_maker/RecipyMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipy.recipy_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipyMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
